dd0a0b33144df57ed15f5bb52ad33cd67325b275
Harshit Bansal <harshitbansal394@gmail.com>
2025-11-03 19:59:03 +0530

    add more practice exercises from Singleton Class

---
+++ src/javacodingpractice/OOPS/singletonclass/Main2.java
@@ -0,0 +1,16 @@
+package javacodingpractice.OOPS.singletonclass;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+	   Singleton2 x = Singleton2.Singleton2();
+	   Singleton2 y = Singleton2.Singleton2();
+	   
+	   x.s = (x.s).toUpperCase();
+	   System.out.println("x : "+x.s+ " y: "+ y.s);
+	   
+	   y.s = (y.s).toLowerCase();
+	   System.out.println("x : "+x.s + " y: "+ y.s);
+	}
+	
+}
---
+++ src/javacodingpractice/OOPS/singletonclass/Singleton1.java
@@ -0,0 +1,22 @@
+package javacodingpractice.OOPS.singletonclass;
+
+public class Singleton1 {
+
+	private static Singleton1 single_instance = null;
+
+	public String s;
+
+	private Singleton1()
+	{
+		s = "Singleton1 class constructor";
+	}
+
+	public static synchronized Singleton1 getInstance()
+	{
+		if(single_instance==null)
+			single_instance = new Singleton1();
+		return single_instance;
+	}
+
+
+}
