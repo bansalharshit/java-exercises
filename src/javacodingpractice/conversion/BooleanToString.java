354ea899a79ff35ecf1cac152d230555acebe28b
Harshit Bansal <harshitbansal394@gmail.com>
2025-11-04 20:16:54 +0530

    add more practice exercises based on conversion and patterns

---
+++ src/javacodingpractice/conversion/BooleanToString.java
@@ -0,0 +1,11 @@
+package javacodingpractice.conversion;
+
+public class BooleanToString {
+
+	public static void main(String[] args) {
+		// there is a toString() Method of Boolean class which convert boolean value into string
+		boolean vlaue = true;
+		String output = Boolean.toString(vlaue);
+		System.out.println(output);
+	}
+}
